e747057898b92f5ba45cffbff391fe870489e0dd
Atharva <atharvakumarsingh968@gmail.com>
2026-08-26 11:59:50 +0530

    Implement twoSum function in Two sum.cpp

---
+++ Array/Two sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i=0;i<= nums.size()-1;i++){
+            for (int j=0;j<= nums.size()-1;j++){
+                if (i==j){
+                    continue;
+                }
+                else if (nums.at(i)+nums.at(j)==target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+            if (!(result.empty())){
+                break;
+            }
+        }
+        return result;
+        
+    }
+};
